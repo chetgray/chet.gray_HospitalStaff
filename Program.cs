7f187406498c1bc45f764ee36e85a1c4bb94ad08
Chet Gray <chet.gray@waystar.com>
2022-09-22 23:21:26 -0400

    ✨ feat: `RegisteredNurse` class
    > - Resident
    >   - Name
    >   - Employee ID
    >   - Hire Date
    >   - Supervising Nurse Supervisor
    
    > - Loop through all of the workers.  For each worker:
    >   - Output the type of worker they are
    >   - If they are a Registered Nurse, output their Nurse Supervisor.

--- Program.cs
+++ Program.cs
@@ -43,6 +43,17 @@ namespace HospitalStaff
             staff.Add(new NurseSupervisor { Name = "Destiny Fonseca", EmployeeID = "91407D8", HireDate = "2021-01-10" });
             staff.Add(new NurseSupervisor { Name = "Grant Dutta", EmployeeID = "3EA1784", HireDate = "2004-08-30" });
 
+            staff.Add(new RegisteredNurse { Name = "Rhona Atmaca", EmployeeID = "7345DB7", HireDate = "2022-05-27", SupervisingNurseSupervisor = (NurseSupervisor)staff[21] });
+            staff.Add(new RegisteredNurse { Name = "Tanisha de Waal", EmployeeID = "5D377F3", HireDate = "2022-03-03", SupervisingNurseSupervisor = (NurseSupervisor)staff[23] });
+            staff.Add(new RegisteredNurse { Name = "Kennedy Schmitt", EmployeeID = "3CEA275", HireDate = "2022-05-26", SupervisingNurseSupervisor = (NurseSupervisor)staff[26] });
+            staff.Add(new RegisteredNurse { Name = "Dante Maler", EmployeeID = "CD32520", HireDate = "2022-03-04", SupervisingNurseSupervisor = (NurseSupervisor)staff[21] });
+            staff.Add(new RegisteredNurse { Name = "Walter Gün", EmployeeID = "A681425", HireDate = "2022-03-21", SupervisingNurseSupervisor = (NurseSupervisor)staff[29] });
+            staff.Add(new RegisteredNurse { Name = "Slade de Azevedo", EmployeeID = "2B68B85", HireDate = "2021-12-13", SupervisingNurseSupervisor = (NurseSupervisor)staff[22] });
+            staff.Add(new RegisteredNurse { Name = "Clarke Biçer", EmployeeID = "C5CAF5B", HireDate = "2021-11-02", SupervisingNurseSupervisor = (NurseSupervisor)staff[23] });
+            staff.Add(new RegisteredNurse { Name = "Riley van Gelder", EmployeeID = "E7BF304", HireDate = "2022-07-20", SupervisingNurseSupervisor = (NurseSupervisor)staff[27] });
+            staff.Add(new RegisteredNurse { Name = "Lois Green", EmployeeID = "5E9796C", HireDate = "2022-08-30", SupervisingNurseSupervisor = (NurseSupervisor)staff[22] });
+            staff.Add(new RegisteredNurse { Name = "Price Aras", EmployeeID = "277AF98", HireDate = "2022-07-21", SupervisingNurseSupervisor = (NurseSupervisor)staff[21] });
+
             foreach (IHospitalWorker worker in staff)
             {
                 Console.Write(
@@ -64,6 +75,12 @@ namespace HospitalStaff
                 {
                     Console.Write(
                         $"  Type: Nurse Supervisor,\tNurses: {nurseSupervisor.SupervisedNursesCount}"
+                    );
+                }
+                if (worker is RegisteredNurse registeredNurse)
+                {
+                    Console.Write(
+                        $"  Type: Registered Nurse,\tSupervisor: {registeredNurse.SupervisingNurseSupervisor.Name}"
                     );
                 }
                 Console.WriteLine();
---
+++ RegisteredNurse.cs
@@ -0,0 +1,21 @@
+﻿namespace HospitalStaff
+{
+    internal class RegisteredNurse : HospitalWorker
+    {
+        private NurseSupervisor _supervisingNurseSupervisor;
+
+        public NurseSupervisor SupervisingNurseSupervisor
+        {
+            get { return _supervisingNurseSupervisor; }
+            set
+            {
+                if (_supervisingNurseSupervisor != null)
+                {
+                    SupervisingNurseSupervisor.SupervisedNursesCount--;
+                }
+                _supervisingNurseSupervisor = value;
+                SupervisingNurseSupervisor.SupervisedNursesCount++;
+            }
+        }
+    }
+}
